480ccacd412b56f243f8d495a7c0bf981ad0f61e
tomfriwel <976932447@qq.com>
2018-03-30 16:47:24 +0800

    test navigation

--- App.js
+++ App.js
@@ -4,20 +4,51 @@
  * @flow
  */
 
-
+import React, { Component } from 'react';
+import {
+    Image,
+    Button
+} from 'react-native';
 import { StackNavigator } from 'react-navigation'
 import Shop from './components/pages/Shop'
 import AnimationTest from './components/pages/AnimationTest'
 import BillDetail from './components/pages/BillDetail'
 
-export default StackNavigator({
-    Home: {
-        screen: Shop,
+class LogoTitle extends React.Component {
+    render() {
+        return (
+            <Image
+                source={require('./assets/images/sample.jpg')}
+                style={{ width: 30, height: 30 }}
+            />
+        );
+    }
+}
+
+export default StackNavigator(
+    {
+        Home: {
+            screen: Shop,
+        },
+        AnimationTest: {
+            screen: AnimationTest,
+        },
+        BillDetail: {
+            screen: BillDetail,
+        }
     },
-    AnimationTest: {
-        screen: AnimationTest,
-    },
-    BillDetail: {
-        screen: BillDetail,
-    },
-})
+    {
+        initialRouteName: 'Home',
+        /* The header config from HomeScreen is now here */
+        navigationOptions: {
+            headerTitle:<LogoTitle />,
+            headerStyle: {
+                backgroundColor: '#aa5100',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        },
+    }
+)
--- components/pages/BillDetail.js
+++ components/pages/BillDetail.js
@@ -10,6 +10,7 @@ import {
     StyleSheet,
     Text,
     View,
+    Button
 } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient'
 
@@ -18,18 +19,61 @@ const Dimensions = require('Dimensions');
 const window = Dimensions.get('window');
 
 export default class AnimationTest extends Component {
+    static navigationOptions = ({ navigation }) => {
+        const { params } = navigation.state;
+
+        return {
+            title: params ? params.title : 'A Nested Details Screen',
+            headerStyle: {
+                backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+                fontFamily: 'Menlo'
+            },
+            headerRight: (
+                <Button
+                    onPress={()=>{
+                        if(params.increaseCount) {
+                            params.increaseCount()
+                        }
+                    }}
+                    title={(params.count?params.count:0)+''}
+                    color="#fff"
+                />
+            ),
+        }
+    };
+
+    componentWillMount() {
+        this.props.navigation.setParams({ increaseCount: this._increaseCount?this._increaseCount:123});
+        this.props.navigation.setParams({ count: 0 });
+    }
+    state = {
+        count: 0,
+    }
+
+    _increaseCount = () => {
+        this.setState({ count: this.state.count + 1 });
+        this.props.navigation.setParams({ count:this.state.count });
+    }
+
+
     constructor(props) {
         super(props)
     }
-    
 
     render() {
-        const { params } = this.props.navigation.state;
-        console.log(params)
+        const { params } = this.props.navigation.state
 
         return (
             <View style={styles.pageContainer}>
                 <Text>{params.title}</Text>
+                <Button
+                    title="Update the title"
+                    onPress={() => this.props.navigation.setParams({ title: 'Updated!' })}
+                />
             </View>
         );
     }
