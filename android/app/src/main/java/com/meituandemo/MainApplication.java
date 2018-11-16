6e9d47585f24df08bbb7c98ddb68984f2db09f6f
tomfriwel <976932447@qq.com>
2018-11-16 13:17:28 +0800

    test

--- android/app/src/main/java/com/meituandemo/MainApplication.java
+++ android/app/src/main/java/com/meituandemo/MainApplication.java
@@ -3,10 +3,13 @@ package com.meituandemo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.BV.LinearGradient.LinearGradientPackage;
+import org.reactnative.camera.RNCameraPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.lwansbrough.RCTCamera.*;
 
 import java.util.Arrays;
 import java.util.List;
@@ -22,7 +25,10 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
+          new MainReactPackage(),
+            new LinearGradientPackage(),
+            new RNCameraPackage(),
+          new RCTCameraPackage()
       );
     }
 
--- components/pages/Home.js
+++ components/pages/Home.js
@@ -10,8 +10,11 @@ import {
     StyleSheet,
     Text,
     View,
-    Button
+    Button,
+    TouchableOpacity
 } from 'react-native';
+
+import QRCodeScanner from 'react-native-qrcode-scanner';
 
 import api from '../../utils/test-api'
 import net from '../../utils/net'
@@ -21,7 +24,7 @@ const window = Dimensions.get('window');
 
 export default class Home extends Component {
     state = {
-        title:"hello android developed using react-native"
+        title: "hello android developed using react-native"
     }
     constructor(props) {
         super(props)
@@ -29,56 +32,95 @@ export default class Home extends Component {
 
     loadData() {
 
-        net.get({
-            url: api.wechat.test
-        }).then(res => {
-            console.log('res:')
-            console.log(res)
-            this.setState({
-                title: "success"
-            })
-        }).catch(res => {
-            console.log('res:')
-            console.log(res)
-            this.setState({
-                title: "fail:"+JSON.stringify(res)
-            })
-        })
+        // net.get({
+        //     url: api.wechat.test
+        // }).then(res => {
+        //     console.log('res:')
+        //     console.log(res)
+        //     this.setState({
+        //         title: "success"
+        //     })
+        // }).catch(res => {
+        //     console.log('res:')
+        //     console.log(res)
+        //     this.setState({
+        //         title: "fail:" + JSON.stringify(res)
+        //     })
+        // })
     }
 
     componentWillMount() {
         this.setState({
             title: "loading"
         })
-        this.loadData()
+        // this.loadData()
     }
 
+    // render() {
+    //     return (
+    //         <View style={styles.pageContainer}>
+    //             <Text>{this.state.title}</Text>
+    //             <Button
+    //                 title="Update the title"
+    //                 onPress={() => {
+    //                     console.log("press")
+    //                     this.setState({
+    //                         title:'press button'
+    //                     })
+    //                 }}
+    //             />
+    //         </View>
+    //     );
+    // }
+    onSuccess(e) {
+        Linking
+            .openURL(e.data)
+            .catch(err => console.error('An error occured', err));
+    }
     render() {
         return (
-            <View style={styles.pageContainer}>
-                <Text>{this.state.title}</Text>
-                <Button
-                    title="Update the title"
-                    onPress={() => {
-                        console.log("press")
-                        this.setState({
-                            title:'press button'
-                        })
-                    }}
-                />
-            </View>
+            <QRCodeScanner
+                onRead={this.onSuccess.bind(this)}
+                topContent={
+                    <Text style={styles.centerText}>
+                        Go to <Text style={styles.textBold}>wikipedia.org/wiki/QR_code</Text> on your computer and scan the QR code.
+              </Text>
+                }
+                bottomContent={
+                    <TouchableOpacity style={styles.buttonTouchable}>
+                        <Text style={styles.buttonText}>OK. Got it!</Text>
+                    </TouchableOpacity>
+                }
+            />
         );
     }
 }
 
 const styles = StyleSheet.create({
-    pageContainer: {
-        backgroundColor: "#fff",
-        position: "absolute",
-        width: window.width,
-        left: 0,
-        top: 20,
-        height: window.height - 20,
-        // backgroundColor: 'red'
-    }
+    // pageContainer: {
+    //     backgroundColor: "#fff",
+    //     position: "absolute",
+    //     width: window.width,
+    //     left: 0,
+    //     top: 20,
+    //     height: window.height - 20,
+    //     // backgroundColor: 'red'
+    // }
+    centerText: {
+        flex: 1,
+        fontSize: 18,
+        padding: 32,
+        color: '#777',
+    },
+    textBold: {
+        fontWeight: '500',
+        color: '#000',
+    },
+    buttonText: {
+        fontSize: 21,
+        color: 'rgb(0,122,255)',
+    },
+    buttonTouchable: {
+        padding: 16,
+    },
 });
